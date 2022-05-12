9e50df8ef4b55462421c628cd08a6009cdf41f6e
Corrado Nadolo-Buzneri <corrado.nadolo.buzneri@gmail.com>
2022-05-12 15:56:36 +0300

    yes

--- homepage_scripts.js
+++ homepage_scripts.js
@@ -9,8 +9,20 @@ function handleClick() {
 
 window.onload=getStarted;
 */
+function onClickCancel(){
+    var x=document.getElementById("login_container");
+    var y=document.getElementById("signup_container");
+    x.style.display="none";
+    y.style.display="none";
+}
+
 function onClickLogin(){
-    var x=document.getElementById("login_container")
+    var x=document.getElementById("login_container");
+    x.style.display="block";
+}
+function onClickSignup(){
+    var x=document.getElementById("signup_container");
+    x.style.display="block";
 }
 
 
